052be8176145799f3345e73197af871efb5263af
GermanRz <germanramirez@outlook.com>
2026-06-24 07:28:52 -0500

    Maquetacion inicial, acceso a menu, inicio, gestion de usuarios y error404

---
+++ vistas/modulos/menu.php
@@ -0,0 +1,148 @@
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <a href="vistas/index3.html" class="brand-link">
+          <img src="vistas/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+          <span class="brand-text font-weight-light">Odin</span>
+      </a>
+
+      <!-- Sidebar -->
+      <div class="sidebar">
+          <!-- Sidebar user (optional) -->
+          <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+              <div class="image">
+                  <img src="vistas/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+              </div>
+              <div class="info">
+                  <a href="#" class="d-block">Administrador</a>
+              </div>
+          </div>
+
+          <!-- Sidebar Menu -->
+          <nav class="mt-2">
+              <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                  <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+
+
+                  <!-- *******************************INICIO******************************* -->
+                  <li class="nav-item">
+                      <a href="inicio" class="nav-link">
+                          <i class="nav-icon fas fa-home"></i>
+                          <p>
+                              Inicio
+                          </p>
+                      </a>
+                  </li>
+
+                  <!-- ******************************* CONTROL DE ACCESO ******************************* -->
+                  <li class="nav-item">
+                      <a href="#" class="nav-link">
+                          <i class="nav-icon fas fa-users"></i>
+                          <p>
+                              Control de Acceso
+                              <i class="fas fa-angle-left right"></i>
+                          </p>
+                      </a>
+                      <ul class="nav nav-treeview">
+                          <li class="nav-item">
+                              <a href="gestion_usuarios" class="nav-link">
+                                  <i class="far fa-circle nav-icon"></i>
+                                  <p>Gestión de usuarios</p>
+                              </a>
+                          </li>
+                          <li class="nav-item">
+                              <a href="../layout/top-nav-sidebar.html" class="nav-link">
+                                  <i class="far fa-circle nav-icon"></i>
+                                  <p>Roles y permisos</p>
+                              </a>
+                          </li>
+                      </ul>
+                  </li>
+
+                  <!-- ******************************* DEPENDENCIAS ******************************* -->
+
+                  <li class="nav-item">
+                      <a href="../widgets.html" class="nav-link">
+                          <i class="nav-icon fas fa-building"></i>
+                          <p>
+                              Dependencias
+                          </p>
+                      </a>
+                  </li>
+
+                  <!-- ******************************* RADICACION ******************************* -->
+
+                  <li class="nav-item">
+                      <a href="../widgets.html" class="nav-link">
+                          <i class="nav-icon fas fa-file"></i>
+                          <p>
+                              Radicación
+                          </p>
+                      </a>
+                  </li>
+
+                  <!-- ******************************* GESTION DOCUMENTAL ******************************* -->
+                  <li class="nav-item">
+                      <a href="#" class="nav-link">
+                          <i class="nav-icon fas fa-eye"></i>
+                          <p>
+                              Gestión documental
+                              <i class="fas fa-angle-left right"></i>
+                          </p>
+                      </a>
+
+                      <ul class="nav nav-treeview">
+                          <li class="nav-item">
+                              <a href="../layout/top-nav.html" class="nav-link">
+                                  <i class="far fa-circle nav-icon"></i>
+                                  <p>Consulta y seguimiento</p>
+                              </a>
+                          </li>
+
+                          <li class="nav-item">
+                              <a href="../layout/top-nav-sidebar.html" class="nav-link">
+                                  <i class="far fa-circle nav-icon"></i>
+                                  <p>Series documentales</p>
+                              </a>
+                          </li>
+
+                          <li class="nav-item">
+                              <a href="../layout/top-nav-sidebar.html" class="nav-link">
+                                  <i class="far fa-circle nav-icon"></i>
+                                  <p>Subseries documentales</p>
+                              </a>
+                          </li>                          
+
+                      </ul>
+                  </li>
+
+                  <!-- ******************************* TRAMITES ******************************* -->
+
+                  <li class="nav-item">
+                      <a href="../widgets.html" class="nav-link">
+                          <i class="nav-icon fas fa-handshake"></i>
+                          <p>
+                              Gestión de trámites
+                          </p>
+                      </a>
+                  </li> 
+                  
+                  <!-- ******************************* REPORTES ******************************* -->
+
+                  <li class="nav-item">
+                      <a href="../widgets.html" class="nav-link">
+                          <i class="nav-icon fas fa-chart-bar"></i>
+                          <p>
+                              Reportes y estadísticas
+                          </p>
+                      </a>
+                  </li>                  
+
+
+
+              </ul>
+          </nav>
+          <!-- /.sidebar-menu -->
+      </div>
+      <!-- /.sidebar -->
+  </aside>
---
+++ vistas/plantilla.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Blank Page</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="vistas/plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="vistas/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition sidebar-mini">
+<!-- Site wrapper -->
+<div class="wrapper">
+    <?php 
+        include "modulos/encabezado.php";
+        include "modulos/menu.php";
+
+
+        if (isset($_GET["ruta"])){
+
+            if(
+                $_GET["ruta"]=="inicio" ||
+                $_GET["ruta"]=="gestion_usuarios" 
+            ){
+                include "modulos/".$_GET["ruta"].".php";
+            }//fin de la lista blanca
+            else{
+                include "modulos/error404.php";
+            }
+
+        } //fin de la captura de la variable ruta
+        include "modulos/inicio.php";
+        
+        
+        include "modulos/footer.php";
+    
+    ?>
+
+
+
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="vistas/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="vistas/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="vistas/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<!-- <script src="vistas/dist/js/demo.js"></script> -->
+</body>
+</html>
