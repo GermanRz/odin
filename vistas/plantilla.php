0a1f2511905e59f58a23c9cf7a96dc4b01ebcb62
GermanRz <germanramirez@outlook.com>
2026-06-24 09:40:43 -0500

    maquetacion inicial modulo de usuarios

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -7,6 +7,13 @@
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">   
+  <!-- DataTables -->
+  <link rel="stylesheet" href="vistas/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="vistas/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="vistas/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">   
+
   <!-- Font Awesome -->
   <link rel="stylesheet" href="vistas/plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
@@ -64,5 +71,33 @@
 <script src="vistas/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
 <!-- <script src="vistas/dist/js/demo.js"></script> -->
+<script src="vistas/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="vistas/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="vistas/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="vistas/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="vistas/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="vistas/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="vistas/plugins/jszip/jszip.min.js"></script>
+<script src="vistas/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="vistas/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="vistas/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="vistas/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="vistas/plugins/datatables-buttons/js/buttons.colVis.min.js"></script> 
+
+
+<script>
+  $(function () {
+    $("#tblUsuarios").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["excel", "pdf"]
+    }).buttons().container().appendTo('#tblUsuarios_wrapper .col-md-6:eq(0)');
+
+  });
+</script>
+
+
+
+
+
 </body>
 </html>
